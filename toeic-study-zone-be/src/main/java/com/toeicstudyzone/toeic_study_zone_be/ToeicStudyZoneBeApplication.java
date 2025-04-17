9355f505437dd1bfc1207bc402723918e9b81b11
Nguyễn Long Vũ <nguyenlongvu22122003@gmail.com>
2025-04-17 11:36:11 +0700

    Thêm 2 folder từ vị trí gốc

---
+++ toeic-study-zone-be/src/main/java/com/toeicstudyzone/toeic_study_zone_be/ToeicStudyZoneBeApplication.java
@@ -0,0 +1,13 @@
+package com.toeicstudyzone.toeic_study_zone_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToeicStudyZoneBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToeicStudyZoneBeApplication.class, args);
+	}
+
+}
---
+++ toeic-study-zone-fe/src/layouts/DefaultLayout.vue
@@ -0,0 +1,41 @@
+<!-- <template>
+  <div class="default-layout">
+    <router-view />
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+.default-layout {
+  min-height: 100vh; /* Đảm bảo layout chiếm toàn bộ chiều cao */
+  display: flex;
+  flex-direction: column;
+}
+</style> -->
+<template>
+  <div class="default-layout">
+    <NavbarClient />
+    <main>
+      <router-view />
+    </main>
+    <FooterClient />
+  </div>
+</template>
+
+<script setup>
+import NavbarClient from "@/components/common/NavbarClient.vue";
+import FooterClient from "@/components/common/FooterClient.vue";
+</script>
+
+<style lang="scss" scoped>
+.default-layout {
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+
+  main {
+    flex: 1;
+  }
+}
+</style>
